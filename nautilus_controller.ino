250c63bc96bd65f1d16ab7c4c5971e984175cf27
Ben Irwin <benlevyirwin@gmail.com>
2026-09-08 17:49:57 -0400

    updated nautilus_controller code

--- nautilus_controller.ino
+++ nautilus_controller.ino
@@ -10,7 +10,7 @@ const int PAN_SERVO = 10;
 
 //other constants
 const long KEEPALIVE_INTERVAL = 500;
-const int MESSAGE_LENGTH = 16;
+const int MESSAGE_LENGTH = 5;
 const int BAUD_RATE = 9600;
 const int PWM_FREQ = 50;
 const int ESC_SWITCHING_DELAY = 500;
@@ -34,28 +34,57 @@ struct MotorState{
   int cam_tilt = 0;
 };
 
-class Thruster {
-  private:
+//class definitions
+
+class ManagedMotor {
+  protected:
     int current_value;  // -99 to 99
     int target_value;   // -99 to 99
     int pin_assignment;
-    unsigned long timer_start = 0;
 
-    void set_thrust() {
-      if (input > 99){input = 99;}
-      if (input < -99){input=-99;}
-      float tgt_time = 1.5 + (input / 99.0 * 0.5); // scaled from 1.0 to 2.0 ms
+    void set_pwm_signal() {
+      float tgt_time = 1.5 + (current_value / 99.0 * 0.5); // scaled from 1.0 to 2.0 ms
       float duty_time = 1000 / PWM_FREQ;
-      pwm_signal = round(tgt_time / duty_time * 255); // scaled from 0 to 255
+      int pwm_signal = round(tgt_time / duty_time * 255); // scaled from 0 to 255
       analogWrite(pin_assignment, pwm_signal);
     }
-
+  
   public:
-    void set_target(int value) {
-      target_value = value;
+    ManagedMotor(int pin_number) {
+      pin_assignment = pin_number;
     }
 
-    void update() {
+    void set_target(int value) {
+      target_value = constrain(value, -99, 99);
+    }
+
+    virtual void update() {
+      if (current_value != target_value){ 
+        current_value = target_value;
+        set_pwm_signal();
+        return;
+      } 
+    }
+
+    void initialize() {
+      analogWriteFrequency(pin_assignment, PWM_FREQ);
+      current_value = 0;
+      set_pwm_signal();
+    }
+};
+
+class Thruster : public ManagedMotor {
+  private:
+    unsigned long timer_start = 0;
+
+  public:
+    Thruster(int pin_number)
+      : ManagedMotor(pin_number)
+      {
+        // empty ctor... :(
+      }
+
+    void update() override {
       if (current_value == target_value){ return; } // no action needed
 
       if (timer_start != 0) { // timer was started - ESC should be in idle but may be used for smooth acceleration later
@@ -63,7 +92,7 @@ class Thruster {
         if (now - timer_start > ESC_SWITCHING_DELAY) {  // timer complete
           timer_start = 0;
           current_value = target_value;
-          set_thrust();
+          set_pwm_signal();
           return;
         } else {
           return;
@@ -73,100 +102,92 @@ class Thruster {
       if ((target_value < 0 && current_value > 0) || (target_value > 0 && current_value < 0)) {  // idle ESC to prevent damage
         current_value = 0;
         timer_start = millis();
-        set_thrust();
+        set_pwm_signal();
         return;
       }
 
       current_value = target_value;
-      set_thrust();
+      set_pwm_signal();
       return;
-    }
-
-    void initialize() {
-      analogWriteFrequency(pin_assignment, PWM_FREQ);
-      current_value = 0;
-      set_thrust();
     }
 };
 
 class MotorController {
 
   private: 
-    MotorState target;
+    enum class ThrusterId {
+      LEFT,
+      RIGHT,
+      DEPTH1,
+      DEPTH2,
+      COUNT
+    };
+
+    enum class ServoId {
+      PAN,
+      TILT,
+      COUNT
+    };
+
+    MotorState target; // may not need these any more
     MotorState current;
-
-    int safe_transition(int i_current, int i_target) {
-      if ((i_current < 0 && i_target > 0) || (i_current > 0 && i_target < 0)) {
-        return 0;
-      } else {
-        return i_target;
-      }
-    }
-
-    int get_pwm_val(int input) {
-      // pulse width = 1000ms / pwm_freq 
-      // 1.0ms = full rev. / 0deg | 1.5ms = neutral / 90deg | 2.0ms = full fwd. / 180deg
-      // teensy analogwrite takes duty cycle values 0-255
-      if (input > 99){input = 99;}
-      if (input < -99){input=-99;}
-      float tgt_time = 1.5 + (input / 99.0 * 0.5); // scaled from 1.0 to 2.0 ms
-      float duty_time = 1000 / PWM_FREQ;
-      return round(tgt_time / duty_time * 255); // scaled from 0 to 255
-    }
+    Thruster thrusters[static_cast<int>(ThrusterId::COUNT)];
+    ManagedMotor servos[static_cast<int>(ServoId::COUNT)];
 
   public:
+
+    MotorController()
+
+      : thrusters{
+        Thruster(LEFT_THRUSTER),
+        Thruster(RIGHT_THRUSTER),
+        Thruster(DEPTH_THRUSTER1),
+        Thruster(DEPTH_THRUSTER2)
+      },
+      servos{
+        ManagedMotor(PAN_SERVO),
+        ManagedMotor(TILT_SERVO)
+      }
+
+      {
+        // empty ctor... :(
+      }
+
+    void set_up() {
+      // set up each thruster and servo - need to call initialize for all motors
+      for (Thruster &thruster : thrusters) {
+        thruster.initialize();
+      }
+      for (ManagedMotor &servo : servos) {
+        servo.initialize();
+      }
+    }
+    
     void set_state(MotorState state) {
       target = state;
+      thrusters[static_cast<int>(ThrusterId::LEFT)].set_target(state.l_thrust);
+      thrusters[static_cast<int>(ThrusterId::RIGHT)].set_target(state.r_thrust);
+      thrusters[static_cast<int>(ThrusterId::DEPTH1)].set_target(state.depth1);
+      thrusters[static_cast<int>(ThrusterId::DEPTH2)].set_target(state.depth2);
+      servos[static_cast<int>(ServoId::PAN)].set_target(state.cam_pan);
+      servos[static_cast<int>(ServoId::TILT)].set_target(state.cam_tilt);
+      delay(3000); // ESC arming delay
     }
 
     void update() {
-      if (current.l_thrust != target.l_thrust) {
-        current.l_thrust = safe_transition(current.l_thrust, target.l_thrust);
-        analogWrite(LEFT_THRUSTER, get_pwm_val(current.l_thrust));
+      for (Thruster &thruster : thrusters) {
+        thruster.update();
       }
-      if (current.r_thrust != target.r_thrust) {
-        current.r_thrust = safe_transition(current.r_thrust, target.r_thrust);
-        analogWrite(RIGHT_THRUSTER, get_pwm_val(current.r_thrust));
-      }
-      if (current.depth1 != target.depth1) {
-        current.depth1 = safe_transition(current.depth1, target.depth1);
-        analogWrite(DEPTH_THRUSTER1, get_pwm_val(current.depth1));
-      }
-      if (current.depth2 != target.depth2) {
-        current.depth2 = safe_transition(current.depth2, target.depth2);
-        analogWrite(DEPTH_THRUSTER2, get_pwm_val(current.depth2));
-      }
-      if (current.cam_pan != target.cam_pan) {
-        current.cam_pan = target.cam_pan;
-        analogWrite(PAN_SERVO, get_pwm_val(current.cam_pan));
-      }
-      if (current.cam_tilt != target.cam_tilt) {
-        current.cam_tilt = target.cam_tilt;
-        analogWrite(TILT_SERVO, get_pwm_val(current.cam_tilt));
-      }
-    }
 
-    void init_thrusters() {
-      //set pwm pin frequency
-      analogWriteFrequency(DEPTH_THRUSTER1, PWM_FREQ);
-      analogWriteFrequency(DEPTH_THRUSTER2, PWM_FREQ);
-      analogWriteFrequency(RIGHT_THRUSTER, PWM_FREQ);
-      analogWriteFrequency(LEFT_THRUSTER, PWM_FREQ);
-      analogWriteFrequency(PAN_SERVO, PWM_FREQ);
-      analogWriteFrequency(TILT_SERVO, PWM_FREQ);
-      delay(5000);
-
-      //unlock esc by setting pwm signal to 1.5ms cycle for 3 sec
-      analogWrite(DEPTH_THRUSTER1, get_pwm_val(0));
-      analogWrite(DEPTH_THRUSTER2, get_pwm_val(0));
-      analogWrite(RIGHT_THRUSTER, get_pwm_val(0));
-      analogWrite(LEFT_THRUSTER, get_pwm_val(0));
-      delay(3000);
+      for (ManagedMotor &servo : servos) {
+        servo.update();
+      }
     }
 };
 
-struct ControlState parse_ctrl(char *str) {
-  struct ControlState state;
+//helper functions
+
+bool parse_ctrl(char *str, ControlState &state) {
   int state_arr[MESSAGE_LENGTH];
 
   char *tok = strtok(str, ",");
@@ -178,13 +199,17 @@ struct ControlState parse_ctrl(char *str) {
     tok = strtok(NULL, ",");
   }
 
+  if (ctr != MESSAGE_LENGTH) { // invalid packet
+    return false;
+  }
+
   state.throttle = state_arr[0];
   state.yaw = state_arr[1];
   state.heave = state_arr[2];
   state.pan = state_arr[3];
   state.tilt = state_arr[4];
 
-  return state
+  return true;
 }
 
 char *read_serial() {
@@ -198,7 +223,7 @@ char *read_serial() {
       msg_pos = 0;
       return msg;
     }
-    else if (msg_pos < MESSAGE_LENGTH) {
+    else if (msg_pos < MESSAGE_LENGTH - 1) {
       msg[msg_pos] = c;
       msg_pos++;
     }
@@ -223,14 +248,14 @@ MotorState calc_motor_state(ControlState ctrl) {
   return motor;
 }
 
+//runtime objects
 static ControlState control_state;
 static MotorController motor_control;
 
 void setup() {
-  motor_control.init_thrusters();
+  motor_control.set_up();
   Serial.begin(BAUD_RATE);
 }
-
 
 void loop() {
   static unsigned long last_msg_time = 0;
@@ -238,10 +263,17 @@ void loop() {
   char *received = read_serial();
 
   if (received != NULL) {
-    last_msg_time = current_time;
-    control_state = parse_ctrl(received);
-    MotorState tgt_state = calc_motor_state(control_state);
-    motor_control.set_state(tgt_state);
+    
+    ControlState incoming;
+    if (parse_ctrl(received, incoming)) {
+      control_state = incoming;
+      
+      MotorState tgt_state = calc_motor_state(control_state);
+      motor_control.set_state(tgt_state);
+
+      last_msg_time = current_time;
+    }
+    
   }
   else if (current_time - last_msg_time > KEEPALIVE_INTERVAL) {
     motor_control.set_state(MotorState{});
